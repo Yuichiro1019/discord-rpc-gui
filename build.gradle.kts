5bc30e7bc295b18e6220a46e1edb33b586f4eabc
Yuichiro1019 <datrix5678@gmail.com>
2026-05-02 16:44:16 +0530

    ight thisis starter template which seems to work and compile

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,33 +1,48 @@
 plugins {
- kotlin("jvm") version "2.1.10"
- id("org.jetbrains.compose") version "1.8.0"
- kotlin("plugin.compose") version "2.1.10"
- kotlin("plugin.serialization") version "2.1.10"
- application
+    kotlin("multiplatform") version "2.3.20"
+    id("org.jetbrains.compose") version "1.10.3"
+    id("org.jetbrains.kotlin.plugin.compose") version "2.3.20"
 }
 
-group = "com.discordrpc"
-version = "1.0.0"
-
-repositories {
-    mavenCentral()
-    maven("https://jitpack.io")
-    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-    google()
-}
-
-dependencies {
- implementation(compose.desktop.currentOs)
- implementation(compose.material3)
- implementation("com.github.caoimhebyrne:KDiscordIPC:0.2.5")
- implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
- testImplementation(kotlin("test"))
-}
-
-application {
-    mainClass.set("com.discordrpc.gui.MainKt")
-}
+group = "com.discordrpc.gui"
+version = "1.0-SNAPSHOT"
 
 kotlin {
-    jvmToolchain(21)
+    jvmToolchain(25)
+
+    jvm("desktop")
+
+    sourceSets {
+        val desktopMain by getting {
+            dependencies {
+                implementation(compose.desktop.currentOs)
+                @Suppress("DEPRECATION")
+                implementation(compose.material3)
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-swing:1.8.0")
+                implementation("com.github.caoimhebyrne:KDiscordIPC:0.2.6")
+                implementation("org.slf4j:slf4j-simple:2.0.12")
+            }
+        }
+    }
+}
+
+compose.desktop {
+    application {
+        mainClass = "com.discordrpc.gui.MainKt"
+
+        jvmArgs += listOf(
+            "-Dawt.toolkit.name=GTK",
+            "-Djava.awt.headless=false",
+            "-Dskiko.renderApi=SOFTWARE"
+        )
+
+        nativeDistributions {
+            targetFormats(
+                org.jetbrains.compose.desktop.application.dsl.TargetFormat.Deb
+            )
+            packageName = "DiscordRPCGUI"
+            packageVersion = "1.0.0"
+        }
+    }
 }
